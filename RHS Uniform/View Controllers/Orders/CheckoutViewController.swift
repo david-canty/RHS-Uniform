9fbd55c1b07aa0c51fffebf6f67f54e94ce98835
David Canty <david.canty@icloud.com>
2018-10-20 19:10:32 +0100

    Checkout work in progress

--- RHS Uniform/View Controllers/Orders/CheckoutViewController.swift
+++ RHS Uniform/View Controllers/Orders/CheckoutViewController.swift
@@ -10,11 +10,7 @@ import UIKit
 
 class CheckoutViewController: UITableViewController {
     
-    @IBOutlet weak var itemsLabel: UILabel!
-    @IBOutlet weak var itemsValueLabel: UILabel!
-    @IBOutlet weak var postageLabel: UILabel!
-    @IBOutlet weak var postageValueLabel: UILabel!
-    @IBOutlet weak var orderTotalValueLabel: UILabel!
+    
     
     override func viewDidLoad() {
         
@@ -27,40 +23,18 @@ class CheckoutViewController: UITableViewController {
         
         super.viewWillAppear(animated)
         
-        updateOrderSummaryLabels()
-    }
-    
-    func updateOrderSummaryLabels() {
-     
-        let itemsTotal = 1
-        itemsLabel.text = "Items: " + String(itemsTotal)
         
-        let itemsValue = 9.99
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .currency
-        let formattedItemsValue = formatter.string(from: itemsValue as NSNumber)
-        itemsValueLabel.text = formattedItemsValue
-        
-        postageLabel.text = "Postage & Packing: " + "Collection Only"
-        
-        let postageValue = 0.00
-        let formattedPostageValue = formatter.string(from: postageValue as NSNumber)
-        postageValueLabel.text = formattedPostageValue
-        
-        let orderTotal = 9.99
-        let formattedTotalValue = formatter.string(from: orderTotal as NSNumber)
-        orderTotalValueLabel.text = formattedTotalValue
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+        
         return 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        
         return 0
     }
 
@@ -74,50 +48,12 @@ class CheckoutViewController: UITableViewController {
     }
     */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
+    // MARK: - Segues
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+
+        
     }
-    */
 
     // MARK: - Button Actions
     
---
+++ RHS Uniform/View Controllers/Orders/OrderSummaryViewController.swift
@@ -0,0 +1,80 @@
+//
+//  OrderSummaryViewController.swift
+//  RHS Uniform
+//
+//  Created by David Canty on 20/10/2018.
+//  Copyright © 2018 ddijitall. All rights reserved.
+//
+
+import UIKit
+
+class OrderSummaryViewController: UITableViewController {
+
+    var numItems: Int = 0
+    var itemsValue: Double = 0.0
+    var postageType: String = ""
+    var postageValue: Double = 0.0
+    var orderTotal: Double = 0.0
+    
+    @IBOutlet weak var itemsLabel: UILabel!
+    @IBOutlet weak var itemsValueLabel: UILabel!
+    @IBOutlet weak var postageLabel: UILabel!
+    @IBOutlet weak var postageValueLabel: UILabel!
+    @IBOutlet weak var orderTotalValueLabel: UILabel!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        
+        updateOrderSummaryLabels()
+    }
+
+    func updateOrderSummaryLabels() {
+        
+        itemsLabel.text = "Items: " + String(numItems)
+
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        let formattedItemsValue = formatter.string(from: itemsValue as NSNumber)
+        itemsValueLabel.text = formattedItemsValue
+
+        postageLabel.text = "Postage & Packing: " + postageType
+
+        let formattedPostageValue = formatter.string(from: postageValue as NSNumber)
+        postageValueLabel.text = formattedPostageValue
+
+        let formattedTotalValue = formatter.string(from: orderTotal as NSNumber)
+        orderTotalValueLabel.text = formattedTotalValue
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        
+        let header = view as! UITableViewHeaderFooterView
+        
+        header.textLabel?.textColor = UIColor(red: 62.0/255.0, green: 60.0/255.0, blue: 146.0/255.0, alpha: 1.0)
+        header.textLabel?.font = UIFont(name: "Arial-BoldMT", size: 16.0)
+        header.textLabel?.text = "Order Summary"
+        //header.textLabel?.frame = header.frame
+        header.textLabel?.textAlignment = NSTextAlignment.left
+    }
+    
+}
