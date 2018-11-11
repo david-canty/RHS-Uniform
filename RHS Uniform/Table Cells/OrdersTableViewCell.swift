58227754eccd091913c9aa99501a6210f4d0db18
David Canty <david.canty@icloud.com>
2018-11-11 20:23:22 +0000

    Orders work in progress

---
+++ RHS Uniform/Table Cells/OrdersTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  OrdersTableViewCell.swift
+//  RHS Uniform
+//
+//  Created by David Canty on 11/11/2018.
+//  Copyright © 2018 ddijitall. All rights reserved.
+//
+
+import UIKit
+
+class OrdersTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        
+        super.awakeFromNib()
+        
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+
+}
